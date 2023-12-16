ceaa595ce8f9337ca9496383a8bda5948d1178af
Paulo Grolli Grabin <paulograbin@gmail.com>
2023-12-16 15:46:02 -0300

    Receive platform directory instead of extensions root directory

--- src/main/java/org/example/workspacewatcher/App.java
+++ src/main/java/org/example/workspacewatcher/App.java
@@ -1,5 +1,7 @@
 package org.example.workspacewatcher;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,17 +11,24 @@ import java.io.IOException;
 @SpringBootApplication
 public class App implements CommandLineRunner {
 
+    public static final String PLATFORM_DIRECTORY = "/home/paulograbin/Hybris/l.k.bennett/hybris/bin/platform";
+
+    private final Logger LOG = LoggerFactory.getLogger(CommandLineRunner.class);
+
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
 
     @Override
     public void run(String... args) {
+        var commandRunner = new CommandRunner();
+        var extensionDirectoryDiscoverer = new ExtensionDirectoryDiscoverer();
+
         try {
-            WorkspaceWatcher ww = new WorkspaceWatcher("/home/paulograbin/Hybris/l.k.bennett/hybris/bin/custom");
+            WorkspaceWatcher ww = new WorkspaceWatcher(PLATFORM_DIRECTORY, extensionDirectoryDiscoverer, commandRunner);
             ww.start();
         } catch (RuntimeException | IOException | InterruptedException e) {
-            System.err.println(e.getMessage());
+            LOG.error("Something went wrong {}", e.getMessage());
         }
     }
 }
